b45cda70a21405974c175a14ff39862eda8da3fe
SameDev <conradtsamuel@gmail.com>
2023-12-23 10:37:31 -0300

    Setup do Sistema em Vue

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///home/samedev/projetos/MagentaStored_desafio/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtPage>
+      <h2>Sistema do Magenta Store</h2>
+      <img src="/public/logo-magenta.png" alt="">
+    </NuxtPage>
+  </div>
+</template>
